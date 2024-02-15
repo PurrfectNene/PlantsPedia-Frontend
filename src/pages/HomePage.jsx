1476db155742c3c0c8c2104e5af3e1092f0636be
Eleanor Pitcher <eleanorpitcher@gmail.com>
2024-02-15 21:47:29 +0100

    basic layout for homepage and navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import homePageImage from '../assets/homepage-image.jpeg'
 
 function HomePage() {
   return (
-    <div className="container d-flex flex-column">
-      <div className="row justify-content-center ">
-          <h1 className="fw-bold text-center col-8 m-5">Cultivate wisdom, nurture your houseplants.</h1>
-      </div>
+    <div className="container-fluid d-flex flex-column">
+      <div className="row justify-content-center align-items-center">
+        <div className='col-md-5 m-5'>
+          <img src={homePageImage} alt="HousePlants" className="mx-auto d-block img-fluid" style={{height: '400px'}} />
+        </div>
+        <div className="col-md-5 text-center">
+          <h1 className="fw-bold m-5">Cultivate wisdom, nurture your houseplants.</h1>
+        </div>
       <div className="row">
         <div className="col px-4 mx-4">
           <Link to="/plants"><h2 className="text-center">View All Plants</h2></Link>
@@ -17,6 +22,7 @@ function HomePage() {
           <p className="text-center">Can't spot your favorite? Grow our collection by adding it.</p>
         </div>
       </div>
+    </div>
     </div>
   )
 }
