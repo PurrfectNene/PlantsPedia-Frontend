8865ea1b38b4273303fdd2287066205485885dfc
Eleanor Pitcher <eleanorpitcher@gmail.com>
2024-02-16 15:35:23 +0100

    some styling done for homepage and navbar layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav className='navbar navbar-expand-lg justify-content-center'>
-      <Link to="/"><ul className="nav-item">PlantsPedia</ul></Link>
+    <nav className='navbar navbar-expand-lg justify-content-center p-3' style={{backgroundColor: '#536847'}}>
+      <Link to="/"><ul className="nav-item fw-bold fs-3 text-center no-underline" style={{color: '#edebe9'}} >PlantsPedia</ul></Link>
     </nav>
   )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,29 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import homePageImage from '../assets/homepage-image.jpeg'
+import homePageImage from '../assets/homepage-cover-image.jpeg'
+import housePlantImage1 from '../assets/houseplant-card-1.jpeg'
+import housePlantImage2 from '../assets/houseplant-card-2.jpeg'
 
 function HomePage() {
   return (
-    <div className="container-fluid d-flex flex-column">
-      <div className="row justify-content-center align-items-center">
-        <div className='col-md-5 m-5'>
-          <img src={homePageImage} alt="HousePlants" className="mx-auto d-block img-fluid" style={{height: '400px'}} />
+    <div style={{backgroundColor: '#E2E2E2'}}>
+      <div className="has-bg-img">
+        <div className="bg-img d-flex align-items-center" style={{backgroundImage: `url(${homePageImage})`, height: '100vh', backgroundSize: 'cover', }}>
+          <div className="container">
+            <div className="row justify-content-end">
+              <div className='col-lg-6' >
+                <h1 className="text-uppercase text-center" style={{fontSize:'5rem', color:'#474443', display: "block"}} >Cultivate wisdom. Nurture your houseplants.</h1>
+            </div>
+          </div>
         </div>
-        <div className="col-md-5 text-center">
-          <h1 className="fw-bold m-5">Cultivate wisdom, nurture your houseplants.</h1>
-        </div>
-      <div className="row">
-        <div className="col px-4 mx-4">
-          <Link to="/plants"><h2 className="text-center">View All Plants</h2></Link>
-          <p className="text-center">Explore our jungle of houseplants to find your green soulmate!</p>
-        </div>
-        <div className="col px-4 mx-4">
-          <Link to="plants/create"><h2 className="text-center">Add a New Plant</h2></Link>
-          <p className="text-center">Can't spot your favorite? Grow our collection by adding it.</p>
+      </div>
+
+      <div className="container mt-5" >
+        <div className="row justify-content-center">
+          <div className="col-md-4 mx-4 mb-5 card" style={{backgroundColor: '#536847', border: 'none'}}>
+            <Link to="/plants"><h2 className="text-center fs-1" style={{color: "#A8A39D"}}>View All Plants</h2></Link>
+            <p className="text-center fs-2 m-4" style={{color: '#edebe9'}} >Explore our jungle of houseplants to find your green soulmate!</p>
+          </div>
+
+          <div className="col-md-4 mx-4 mb-5 card" style={{backgroundColor: '#536847', border: "none"}} >
+            <Link to="plants/create"><h2 className="text-center fs-1" style={{color: "#A8A39D"}}>Add a New Plant</h2></Link>
+            <p className="text-center fs-2 m-4" style={{color: '#edebe9'}} >Can't spot your favorite? Grow our collection by adding it.</p>
+          </div>
         </div>
       </div>
     </div>
-    </div>
+  </div>
+        
   )
 }
 
