f428ea5eaae727b264b192da900530b72a267409
Irene <amoreperdesi@gmail.com>
2024-02-17 16:44:28 +0100

    link added

--- src/pages/AllPlantsPage.jsx
+++ src/pages/AllPlantsPage.jsx
@@ -8,7 +8,7 @@ function AllPlantsPage() {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:5005/Plants`)
+      .get(`http://localhost:5005/plants`)
       .then((plantsFromAPI) => {
         setplants(plantsFromAPI.data)
         console.log(plantsFromAPI.data);
@@ -24,9 +24,12 @@ function AllPlantsPage() {
         <h1 className="text-center">Plants list</h1>
         <div className="row row-cols-1 row-cols-md-3 g-6 mb-4">
           {plants.map((onePlant) => (
+            
             <div className="col mb-4" key={onePlant.id}>
               <div className="card square-card h-100">
+              <Link to={`/projects/${onePlant.id}`}>
                 <img src={onePlant.image} className="card-img-top img-fluid" alt={onePlant.name} style={{ height: "600px", objectFit: "cover" }} />
+              </Link>
                 <div className="card-body">
                   <h2 className="card-title text-center">{onePlant.name}</h2>
                 </div>
