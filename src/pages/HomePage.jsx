1f9dfc01989acf60870b802685ca1664917cd61d
Eleanor Pitcher <eleanorpitcher@gmail.com>
2024-02-17 16:53:14 +0100

    homepage styling updated

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -28,20 +28,6 @@ function HomePage() {
       <div>
         <TopPlantPicks/>
       </div>
-
-      {/* <div className="container-fluid p-5" style={{backgroundColor: '#98977f'}} >
-        <div className="row justify-content-center">
-          <div className="col-md-4 mx-4 mb-5 card pt-4" style={{backgroundColor: '#bdc6b3', border: 'none'}}>
-            <Link to="/plants"><h2 className="text-center fs-1 no-underline" style={{color: "#214624", letterSpacing: '-4px'}}>View All Plants</h2></Link>
-            <p className="text-center fs-2 m-4" style={{color: '#A8A39D', letterSpacing: '-3px'}} >Explore our jungle of houseplants to find your green soulmate!</p>
-          </div>
-
-          <div className="col-md-4 mx-4 mb-5 card pt-4" style={{backgroundColor: '#bdc6b3', border: "none"}} >
-            <Link to="plants/create"><h2 className="text-center fs-1 no-underline" style={{color: "#214624", letterSpacing: '-4px'}}>Add a New Plant</h2></Link>
-            <p className="text-center fs-2 m-4" style={{color: '#A8A39D', letterSpacing: '-3px'}} >Can't spot your favorite? Grow our collection by adding it.</p>
-          </div>
-        </div>
-      </div> */}
     </div>
   </div>
         
