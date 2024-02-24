4542b3b63c2db677708de37b94e15d9d9dddedcb
Irene <amoreperdesi@gmail.com>
2024-02-24 17:47:49 +0100

    navbar menu style improved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,27 +23,6 @@ function Navbar() {
       </div>
     </nav>
     </div>
-
-    // <div>
-    //   <nav className='navbar navbar-expand-lg p-4' style={{ fontFamily: 'Be Vietnam Pro,Inter,system-ui,sans', backgroundColor: '#536847'}}>
-    //   <div className='container-fluid' >
-    //     <Link to="/" className="navbar-brand fw-bold nav-link fs-3" style={{ letterSpacing: '-1px', color: 'white' }}>PlantsPedia</Link>
-    //     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    //       <span className="navbar-toggler-icon"></span>
-    //     </button>
-    //   </div>
-    //   <div className="collapse navbar-collapse" id="navbarNavDropdown">
-    //     <ul className="navbar-nav ms-auto d-flex flex-lg-row justify-content-lg-end ">
-    //       <li className="nav-item col text-nowrap ms-auto">
-    //         <Link to="/plants" className="nav-link px-4 fw-bold" style={{color: 'white'}} >All Plants</Link>
-    //       </li>
-    //       <li className="nav-item col text-nowrap ms-auto">
-    //         <Link to="/plants/create" className="nav-link px-4 fw-bold" style={{color: 'white'}} >Add New Plant</Link>
-    //       </li>
-    //     </ul>
-    //   </div>
-    // </nav>
-    // </div>
     
   )
 }
