3969d66e3224073902f6e31cfc812af65d73b42c
Eleanor Pitcher <eleanorpitcher@gmail.com>
2024-02-29 21:28:43 +0100

    comment section functionality complete

--- src/pages/PlantDetailsPage.jsx
+++ src/pages/PlantDetailsPage.jsx
@@ -10,20 +10,40 @@ function PlantDetailsPage() {
   const [plant, setPlant] = useState(null)
   const navigate = useNavigate()
 
-  useEffect(() => {
-    axios.get(`http://localhost:5005/plants/${plantId}`)
+
+  function getPlant(){
+    axios.get(`http://localhost:5005/plants/${plantId}?_embed=comments`)
     .then((response) => {
       setPlant(response.data)
     })
     .catch((err)=> {
       console.log(err)
     })
+  }
+  useEffect(() => {
+    getPlant()
   }, [plantId])
 
   function deletePlant(){
     axios.delete(`http://localhost:5005/plants/${plantId}`)
     .then(()=>{
       navigate('/plants')
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
+  const [commentName, setCommentName] = useState('')
+  const [comment, setComment] = useState('')
+
+  function handleSubmit(e){
+    e.preventDefault()
+    const newComment = {name:commentName, commentBody:comment, plantId:parseInt(plantId)}
+    axios.post(`http://localhost:5005/comments`, newComment)
+    .then((response)=>{
+      console.log(response.data)
+      getPlant()
     })
     .catch((err)=>{
       console.log(err)
@@ -56,11 +76,30 @@ function PlantDetailsPage() {
                 <h2>Where should I live?</h2>
                 <p>{plant.outdoor_or_indoor}</p>
               </div>
-              <div className='d-flex justify-content-center mb-5'>
+              <div className='px-5 mb-5'>
                 <Link to={`/plants/${plant.id}/edit`}>
                 <button className="m-3 btn btn-outline-dark">Edit me</button>
                 </Link>
                 <button className="m-3 btn btn-outline-dark" onClick={deletePlant}>Delete me</button>
+              </div>
+              <div className="flex-column d-flex p-5 mb-5">
+                <h3>Got something to say about the {plant.name}? Add a comment below to help others:</h3>
+                <form onSubmit={handleSubmit}>
+                  <h4>Name:</h4>
+                  <input type="text" value={commentName} onChange={(e) =>setCommentName(e.target.value)} />
+                  <h4>Comment:</h4>
+                  <input type="text" value={comment} onChange={(e) =>setComment(e.target.value)} />
+                  <button>Submit</button>
+                </form>
+
+                <h3>Comments from other users</h3>
+                <div>
+                  {plant.comments.map((oneComment)=>(
+                    <div key={oneComment.id}>
+                      <p>{oneComment.name}: {oneComment.commentBody}</p>
+                    </div>
+                  ))}
+                </div>
               </div>
               
               <SimilarPlants plant={plant}></SimilarPlants>
